e5134c2a0d1dd9adadc86d2959d7eb3119736f09
Dominic Lee <dominictwlee@gmail.com>
2020-12-16 23:12:25 +0000

    chore: setup jest config

---
+++ src/utils/testUtils.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ThemeProvider } from '../theming';
+
+const Providers = ({ children }: any) => {
+  return <ThemeProvider theme="light">{children}</ThemeProvider>;
+};
+
+const customRender = (ui: any, options: any) =>
+  render(ui, { wrapper: Providers, ...options });
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export { customRender as render };
