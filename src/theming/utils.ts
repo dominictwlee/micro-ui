46201c2626aec3ef6ab6ce382a300fa704ce77a8
Dominic Lee <dominictwlee@gmail.com>
2020-12-16 17:40:03 +0000

    feat: type check theme object values

--- src/theming/utils.ts
+++ src/theming/utils.ts
@@ -1,18 +1,26 @@
+const INVALID_CSS_ERROR = new Error('Invalid CSS value: Expects a string');
+
 export function toCssVarNames<T>(themeObj: T, prefix = '-'): T {
   const vars: any = {};
   for (const [key, value] of Object.entries(themeObj)) {
     const varName = `${prefix}-${key}`;
     if (typeof value === 'object') {
       vars[key] = toCssVarNames(value, varName);
-    } else {
+    } else if (typeof value === 'string') {
       vars[key] = `var(${varName})`;
+    } else {
+      throw INVALID_CSS_ERROR;
     }
   }
   return vars;
 }
 
+interface CssVarMap {
+  [cssVarName: string]: string;
+}
+
 export function toCssVars<T>(themeObj: T, prefix = '-') {
-  const vars: any = {};
+  const vars: CssVarMap = {};
 
   for (const [key, value] of Object.entries(themeObj)) {
     const varName = `${prefix}-${key}`;
@@ -21,8 +29,10 @@ export function toCssVars<T>(themeObj: T, prefix = '-') {
       for (const [nestedKey, nestedValue] of Object.entries(nestedVars)) {
         vars[nestedKey] = nestedValue;
       }
-    } else {
+    } else if (typeof value === 'string') {
       vars[varName] = value;
+    } else {
+      throw INVALID_CSS_ERROR;
     }
   }
   return vars;
