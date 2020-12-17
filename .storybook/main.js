96b4760bf222d3d447d21861c600f3937f08f326
Dominic Lee <dominictwlee@gmail.com>
2020-12-17 22:15:00 +0000

    chore: config emotion css prop for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: [
     '../src/**/*.stories.@(ts|tsx|js|jsx)',
@@ -6,6 +8,13 @@ module.exports = {
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
-    check: true, // type-check stories during Storybook build
+    check: false, // type-check stories during Storybook build
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules[0].use[0].options.presets.push(
+      require.resolve('@emotion/babel-preset-css-prop')
+    );
+
+    return config;
   },
 };
