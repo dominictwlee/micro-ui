3d751b32f54ee86b53192dfe19c98fac8b318f58
Dominic Lee <dominictwlee@gmail.com>
2020-12-17 10:24:20 +0000

    chore: add src story paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,11 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
+  stories: [
+    '../src/**/*.stories.@(ts|tsx|js|jsx)',
+    '../stories/**/*.stories.@(ts|tsx|js|jsx)',
+  ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
-  }
+  },
 };
