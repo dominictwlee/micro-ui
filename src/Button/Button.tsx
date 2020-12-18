dcc7f299d27a7a58ff9a4e02317653c004bc45c8
Dominic Lee <dominictwlee@gmail.com>
2020-12-18 11:18:09 +0000

    feat: add button active effects

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -7,8 +7,7 @@ interface ButtonProps extends HTMLProps<HTMLButtonElement> {
   type?: 'button' | 'submit' | 'reset';
 }
 
-const border = css`
-  border-color: none;
+const borderRadius = css`
   border-radius: ${themeVars.shape.borderRadius[1]};
 `;
 
@@ -20,9 +19,9 @@ const base = css`
   text-decoration: none;
   color: ${themeVars.colors.text.primary};
   padding: 10px;
-  width: 50%;
+  border: 0;
   cursor: pointer;
-  ${border};
+  ${borderRadius}
 
   &:before {
     content: '';
@@ -30,16 +29,20 @@ const base = css`
     width: 100%;
     height: 100%;
     opacity: 0;
-    transition: opacity 0.3s;
+    transition: opacity, background 0.3s;
     top: 0;
     left: 0;
     background: ${themeVars.colors.primary.light};
-    ${border}
+    ${borderRadius}
   }
 
   &:hover:before,
-  &:active:before {
+  &:focus:before {
     opacity: 1;
+  }
+
+  &:active:before {
+    background: ${themeVars.colors.primary.dark};
   }
 `;
 
@@ -47,24 +50,18 @@ const buttonTextLayer = css`
   position: relative;
 `;
 
-const primary = css`
-  border-color: ${themeVars.colors.primary.main};
-  background-color: ${themeVars.colors.primary.main};
+const colorStyles = ({ color = 'primary' }: ButtonProps) => css`
+  border-color: ${themeVars.colors[color].main};
+  background-color: ${themeVars.colors[color].main};
 `;
 
-const colorStyles = {
-  primary,
-  secondary: primary,
-  tertiary: primary,
-};
-
 export default function Button({
-  color = 'primary',
+  color,
   children,
   ...props
 }: PropsWithChildren<ButtonProps>) {
   return (
-    <button type="button" css={[base, colorStyles[color]]} {...props}>
+    <button type="button" css={[base, colorStyles]} {...props}>
       <span css={buttonTextLayer}>{children}</span>
     </button>
   );
