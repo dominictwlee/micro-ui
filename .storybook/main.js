10c615339615f3d377d0ff57ebb1c070f06649b7
Dominic Lee <dominictwlee@gmail.com>
2020-12-18 11:17:30 +0000

    chore: simplify storybook presets

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,13 +11,9 @@ module.exports = {
     check: false, // type-check stories during Storybook build
   },
   webpackFinal: async (config, { configType }) => {
-    config.module.rules[0].use[0].options.presets = [
-      require.resolve('@babel/preset-react'),
-      require.resolve('@babel/preset-env'),
-      // Emotion preset must run BEFORE reacts preset to properly convert css-prop.
-      // Babel preset-ordering runs reversed (from last to first). Emotion has to be after React preset.
-      require.resolve('@emotion/babel-preset-css-prop'),
-    ];
+    config.module.rules[0].use[0].options.presets.push(
+      require.resolve('@emotion/babel-preset-css-prop')
+    );
 
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
