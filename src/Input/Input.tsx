8b3bd61522690280dad4b7884fdfd35f5ed1a2d8
Dominic Lee <dominictwlee@gmail.com>
2020-12-19 21:38:48 +0000

    feat: add border transition on focus

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -11,15 +11,20 @@ interface InputProps extends HTMLProps<HTMLInputElement> {
 
 const inputBase = css`
   border-radius: ${themeVars.shape.borderRadius[1]};
-  border: 1px solid ${themeVars.colors.border.light};
+  border-style: solid;
+  border-width: 1px;
+  border-color: ${themeVars.colors.border.light};
   color: ${themeVars.colors.text.primary};
   padding: ${themeVars.spacing[1]};
   background: transparent;
   outline: none;
+  box-shadow: 0 0 0 1px rgba(71, 217, 251, 0);
+  transition: border-color, box-shadow 150ms ease-in;
 `;
 
 const focused = ({ color = 'primary' }: InputProps) => css`
   &:focus {
+    transition-timing-function: ease-out;
     border-color: ${themeVars.colors[color].lighter};
     box-shadow: 0 0 0 1px ${themeVars.colors[color].lighter};
   }
