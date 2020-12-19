1f4d475f63e0f84a4fa9f5d013ff4954c32ac787
Dominic Lee <dominictwlee@gmail.com>
2020-12-19 18:37:49 +0000

    feat: basic styling for Input

---
+++ src/Input/Input.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import Input from './Input';
+
+const meta = {
+  title: 'Input',
+  component: Input,
+  parameters: {
+    controls: { expanded: true },
+  },
+};
+
+export default meta;
+
+const Template = (args: any) => <Input {...args} />;
+
+// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
+// https://storybook.js.org/docs/react/workflows/unit-testing
+export const Default: any = Template.bind({});
---
+++ src/Input/Input.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import { themeVars } from '../theming';
+import { Color } from '../types';
+
+interface InputProps {
+  color?: Color;
+}
+
+const inputBase = css`
+  border-radius: ${themeVars.shape.borderRadius[1]};
+  border: 1px solid ${themeVars.colors.border.light};
+  color: ${themeVars.colors.text.primary};
+  padding: ${themeVars.spacing[1]};
+  background: transparent;
+  outline: none;
+`;
+
+const focused = ({ color = 'primary' }: InputProps) => css`
+  &:focus {
+    border-color: ${themeVars.colors[color].lighter};
+    box-shadow: 0 0 0 1px ${themeVars.colors[color].lighter};
+  }
+`;
+
+export default function Input() {
+  return (
+    <div>
+      <input type="text" css={[inputBase, focused]} />
+    </div>
+  );
+}
